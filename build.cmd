5a4e52d198ef9e7d64f573d644b12cfb01cb402e
Fabricio Catae <fabricio.catae@microsoft.com>
2017-02-02 15:00:35 -0200

    Fix some build process

--- build.cmd
+++ build.cmd
@@ -3,22 +3,22 @@ dotnet restore
 mkdir bin
 
 cd src\Arda.Kanban
-dotnet publish -o .\bin\publish
-xcopy /e /y .\bin\publish ..\..\bin\Arda.Kanban
+dotnet publish
+xcopy /e /y .\bin\debug\netcoreapp1.0\publish ..\..\bin\Arda.Kanban
 cd ..\..
 
 cd src\Arda.Main
-dotnet publish -o .\bin\publish
-xcopy /e /y .\bin\publish ..\..\bin\Arda.Main
+dotnet publish
+xcopy /e /y .\bin\debug\netcoreapp1.0\publish ..\..\bin\Arda.Main
 cd ..\..
 
 cd src\Arda.Permissions
-dotnet publish -o .\bin\publish
-xcopy /e /y .\bin\publish ..\..\bin\Arda.Permissions
+dotnet publish
+xcopy /e /y .\bin\debug\netcoreapp1.0\publish ..\..\bin\Arda.Permissions
 cd ..\..
 
 cd src\Arda.Reports
-dotnet publish -o .\bin\publish
-xcopy /e /y .\bin\publish ..\..\bin\Arda.Reports
+dotnet publish
+xcopy /e /y .\bin\debug\netcoreapp1.0\publish ..\..\bin\Arda.Reports
 cd ..\..
 
