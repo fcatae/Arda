5e80d6ef0e2070e14a8f0f13c6bd82f0886308f8
Fabricio Catae <fabricio.catae@microsoft.com>
2017-02-15 10:36:35 -0200

    Fix #27: Allow the program to choose the TCP port to listent to

--- src/Arda.Reports/Program.cs
+++ src/Arda.Reports/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using System.IO;
 
 namespace Arda.Reports
@@ -7,13 +8,31 @@ namespace Arda.Reports
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", true)
+                            .AddJsonFile("local-secret.json", true)
+                            .AddJsonFile("microservices.json", true)
+                            .AddEnvironmentVariables()
+                            .AddCommandLine(args)
+                            .Build();
+
+            var builder = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://0.0.0.0:8083")
                 .UseIISIntegration()
-                .UseUrls("http://0.0.0.0:80","http://0.0.0.0:83")
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
+
+            string kestrelListen = config["KESTREL_LISTEN_REPORTS"];
+            if (kestrelListen != null)
+            {
+                System.Console.WriteLine("KESTREL_LISTEN_REPORTS = " + kestrelListen);
+
+                builder = builder.UseUrls(kestrelListen);
+            }
+
+            var host = builder.Build();
 
             host.Run();
         }
