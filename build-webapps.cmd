adba68bc48baa65287c6b467ee10cad3ee0b411c
Fabricio Catae <fabricio.catae@microsoft.com>
2017-02-16 14:38:50 -0200

    update nuget package

--- build-webapps.cmd
+++ build-webapps.cmd
@@ -1,5 +1,5 @@
+set NUGET_PACKAGES=D:\home\site\.nuget
 set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=false
-set NUGET_PACKAGES=
 
 dotnet restore -p .\src\Arda.Common\project.json
 
@@ -13,4 +13,3 @@ xcopy /e /y .\deploy\webapp\app_data\*.*  %WEBROOT_PATH%\app_data\
 copy .\deploy\webapp\*.cmd  %WEBROOT_PATH%\
 copy .\deploy\webapp\*.config  %WEBROOT_PATH%\
 
-set NUGET_PACKAGES=D:\home\.nuget 
--- deploy/webapp/webapp-main.cmd
+++ deploy/webapp/webapp-main.cmd
@@ -1,5 +1,5 @@
 cd %HOME%\site\wwwroot\src\Arda.Main
 
-set NUGET_PACKAGES=D:\home\.nuget
+set NUGET_PACKAGES=D:\home\site\.nuget
 
 dotnet run
