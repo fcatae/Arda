f07c7a1b5c2eb974769627d77d505156f6c8d370
Fabricio Catae <fabricio.catae@microsoft.com>
2017-02-16 17:28:00 -0200

    dotnet restore - set package path

--- build-webapps.cmd
+++ build-webapps.cmd
@@ -2,7 +2,7 @@ REM set NUGET_PACKAGES=D:\home\site\.nuget
 
 set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=false
 
-dotnet restore
+dotnet restore --packages d:\home\.nuget -v Debug
 
 dotnet build .\src\Arda.Main\project.json
 
