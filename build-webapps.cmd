0f1ec24f583ef0f531b0e793abb406b608cbc740
Fabricio Catae <fabricio.catae@microsoft.com>
2017-02-16 10:59:20 -0200

    updated the restore

--- build-webapps.cmd
+++ build-webapps.cmd
@@ -1,3 +1,6 @@
+set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=false
+set NUGET_PACKAGES=
+
 dotnet restore -p .\src\Arda.Main\project.json
 dotnet build -p .\src\Arda.Main\project.json
 
@@ -7,3 +10,5 @@ xcopy /e /y .\deploy\webapp\app_data\*.*  %WEBROOT_PATH%\app_data\
 
 copy .\deploy\webapp\*.cmd  %WEBROOT_PATH%\
 copy .\deploy\webapp\*.config  %WEBROOT_PATH%\
+
+set NUGET_PACKAGES=D:\home\.nuget 
