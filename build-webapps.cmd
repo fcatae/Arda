cfd982e4e6725c2025a9266484d32e752cc3945e
Fabricio Catae <fcatae@users.noreply.github.com>
2017-02-17 09:49:41 -0200

    Webapp: Improve the deployment experience (#29)
    * Add command line customization to Kestrel startup
    
    * add more debug information
    
    * Add command line back
    
    * Adjust the fix to run in WebApp
    
    * IIS Integration takes precedence over default port
    
    * Fix #27: Allow the program to choose the TCP port to listent to
    
    * remove uneccessary file
    
    * update netcore version in deploy script
    
    * change the logic
    
    * webapp
    
    * ignore app_data at root
    
    * update .gitignore
    
    * update build webapp
    
    * copy scripts to run
    
    * update deployment settings
    
    * move the files
    
    * update webjob script
    
    * dotnet restore
    
    * dotnet restore in root
    
    * upload config
    
    * update main
    
    * update the script for main
    
    * improve the speed
    
    * fix the path
    
    * updated the restore
    
    * removed techsummit message
    
    * update common
    
    * update nuget_packages
    
    * update nuget package
    
    * set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
    
    * update nuget
    
    * build
    
    * web.config update
    
    * dotnet restore - set package path

--- build-webapps.cmd
+++ build-webapps.cmd
@@ -1,6 +1,15 @@
-dotnet restore
+REM set NUGET_PACKAGES=D:\home\site\.nuget
 
-xcopy /e /y .\deploy\webapp\app_data\*.*  %WEBROOT_PATH%\app_data\
+set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=false
+
+dotnet restore --packages d:\home\.nuget -v Debug
+
+dotnet build .\src\Arda.Main\project.json
 
 copy .\deploy\webapp\*.cmd  %WEBROOT_PATH%\
 copy .\deploy\webapp\*.config  %WEBROOT_PATH%\
+copy .\deploy\webapp\appsettings.json  %WEBROOT_PATH%\
+copy .\deploy\webapp\arda.pfx  %WEBROOT_PATH%\
+
+xcopy /e /y .\deploy\webapp\app_data\*.*  %WEBROOT_PATH%\app_data\
+
