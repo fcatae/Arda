6ef71f0aa7ea2313dac2fb8a4dacbc4a84dae85f
Fabricio Catae <fcatae@microsoft.com>
2017-02-20 16:38:39 -0300

    Remove exception: metric repository

--- src/Arda.Kanban/Repositories/MetricRepository.cs
+++ src/Arda.Kanban/Repositories/MetricRepository.cs
@@ -20,34 +20,27 @@ namespace Arda.Kanban.Repositories
         // Adds a new metric to the system.
         public bool AddNewMetric(MetricViewModel metric)
         {
-            try
+            var fy = _context.FiscalYears.FirstOrDefault(f => f.FiscalYearID == metric.FiscalYearID);
+
+            var metricToBeSaved = new Metric()
             {
-                var fy = _context.FiscalYears.FirstOrDefault(f => f.FiscalYearID == metric.FiscalYearID);
+                MetricID = metric.MetricID,
+                MetricCategory = metric.MetricCategory,
+                MetricName = metric.MetricName,
+                Description = metric.Description,
+                FiscalYear = fy
+            };
 
-                var metricToBeSaved = new Metric()
-                {
-                    MetricID = metric.MetricID,
-                    MetricCategory = metric.MetricCategory,
-                    MetricName = metric.MetricName,
-                    Description = metric.Description,
-                    FiscalYear = fy
-                };
+            _context.Metrics.Add(metricToBeSaved);
+            var response = _context.SaveChanges();
 
-                _context.Metrics.Add(metricToBeSaved);
-                var response = _context.SaveChanges();
-
-                if (response > 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+            if (response > 0)
+            {
+                return true;
             }
-            catch (Exception)
+            else
             {
-                throw;
+                return false;
             }
         }
 
@@ -55,165 +48,109 @@ namespace Arda.Kanban.Repositories
         //TODO: Remove Nullable Casting
         public List<MetricViewModel> GetAllMetrics()
         {
-            try
-            {
-                var response = (from m in _context.Metrics
-                                join f in _context.FiscalYears on m.FiscalYear.FiscalYearID equals f.FiscalYearID
-                                orderby f.FullNumericFiscalYear, m.MetricCategory
-                                 select new MetricViewModel
-                                 {
-                                     MetricID = m.MetricID,
-                                     MetricCategory = m.MetricCategory,
-                                     MetricName = m.MetricName,
-                                     Description = m.Description,
-                                     FiscalYearID = m.FiscalYear.FiscalYearID,
-                                     FullNumericFiscalYear = m.FiscalYear.FullNumericFiscalYear,
-                                     TextualFiscalYear = m.FiscalYear.TextualFiscalYear
-                                 }).ToList();
-                
-                if (response != null)
-                {
-                    return response;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception)
-            {
-                return null;
-            }
+            var response = (from m in _context.Metrics
+                            join f in _context.FiscalYears on m.FiscalYear.FiscalYearID equals f.FiscalYearID
+                            orderby f.FullNumericFiscalYear, m.MetricCategory
+                            select new MetricViewModel
+                            {
+                                MetricID = m.MetricID,
+                                MetricCategory = m.MetricCategory,
+                                MetricName = m.MetricName,
+                                Description = m.Description,
+                                FiscalYearID = m.FiscalYear.FiscalYearID,
+                                FullNumericFiscalYear = m.FiscalYear.FullNumericFiscalYear,
+                                TextualFiscalYear = m.FiscalYear.TextualFiscalYear
+                            }).ToList();
+
+            return response;
         }
 
         // Return all metrics by year
         //TODO: Remove Nullable Casting
         public List<MetricViewModel> GetAllMetrics(int year)
         {
-            try
-            {
-                var response = (from m in _context.Metrics
-                         join f in _context.FiscalYears on m.FiscalYear.FiscalYearID equals f.FiscalYearID
+            var response = (from m in _context.Metrics
+                            join f in _context.FiscalYears on m.FiscalYear.FiscalYearID equals f.FiscalYearID
                             where f.FullNumericFiscalYear == year
-                         orderby f.FullNumericFiscalYear, m.MetricCategory
-                         select new MetricViewModel
-                         {
-                             MetricID = m.MetricID,
-                             MetricCategory = m.MetricCategory,
-                             MetricName = m.MetricName,
-                             Description = m.Description,
-                             FiscalYearID = m.FiscalYear.FiscalYearID,
-                             FullNumericFiscalYear = m.FiscalYear.FullNumericFiscalYear,
-                             TextualFiscalYear = m.FiscalYear.TextualFiscalYear
-                         }).ToList();
-                
-                if (response != null)
-                {
-                    return response;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception)
-            {
-                return null;
-            }
+                            orderby f.FullNumericFiscalYear, m.MetricCategory
+                            select new MetricViewModel
+                            {
+                                MetricID = m.MetricID,
+                                MetricCategory = m.MetricCategory,
+                                MetricName = m.MetricName,
+                                Description = m.Description,
+                                FiscalYearID = m.FiscalYear.FiscalYearID,
+                                FullNumericFiscalYear = m.FiscalYear.FullNumericFiscalYear,
+                                TextualFiscalYear = m.FiscalYear.TextualFiscalYear
+                            }).ToList();
+
+            return response;
         }
 
         // Return metric based on ID
         public MetricViewModel GetMetricByID(Guid id)
         {
-            try
-            {
-                var metric = (from m in _context.Metrics
-                              join f in _context.FiscalYears on m.FiscalYear.FiscalYearID equals f.FiscalYearID
-                              where m.MetricID == id
-                              select new MetricViewModel
-                              {
-                                  MetricID = m.MetricID,
-                                  MetricCategory = m.MetricCategory,
-                                  MetricName = m.MetricName,
-                                  Description = m.Description,
-                                  FiscalYearID = (Guid)m.FiscalYear.FiscalYearID,
-                                  FullNumericFiscalYear = (int)m.FiscalYear.FullNumericFiscalYear,
-                                  TextualFiscalYear = (string)m.FiscalYear.TextualFiscalYear
-                              }).First();
+            var metric = (from m in _context.Metrics
+                          join f in _context.FiscalYears on m.FiscalYear.FiscalYearID equals f.FiscalYearID
+                          where m.MetricID == id
+                          select new MetricViewModel
+                          {
+                              MetricID = m.MetricID,
+                              MetricCategory = m.MetricCategory,
+                              MetricName = m.MetricName,
+                              Description = m.Description,
+                              FiscalYearID = (Guid)m.FiscalYear.FiscalYearID,
+                              FullNumericFiscalYear = (int)m.FiscalYear.FullNumericFiscalYear,
+                              TextualFiscalYear = (string)m.FiscalYear.TextualFiscalYear
+                          }).First();
 
-                if (metric != null)
-                {
-                    return metric;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return metric;
         }
 
         // Update metric data based on ID
         public bool EditMetricByID(MetricViewModel metric)
         {
-            try
+            var metricToBeUpdated = (from m in _context.Metrics
+                                     where m.MetricID == metric.MetricID
+                                     select m).First();
+
+            var fyToBeIncluded = (from fy in _context.FiscalYears
+                                  where fy.FiscalYearID == metric.FiscalYearID
+                                  select fy).First();
+
+
+            if (metricToBeUpdated != null)
             {
-                var metricToBeUpdated = (from m in _context.Metrics
-                                         where m.MetricID == metric.MetricID
-                                         select m).First();
+                // Update informations of object
+                metricToBeUpdated.MetricName = metric.MetricName;
+                metricToBeUpdated.MetricCategory = metric.MetricCategory;
+                metricToBeUpdated.Description = metric.Description;
+                metricToBeUpdated.FiscalYear = fyToBeIncluded;
 
-                var fyToBeIncluded = (from fy in _context.FiscalYears
-                                      where fy.FiscalYearID == metric.FiscalYearID
-                                      select fy).First();
-                
-
-                if (metricToBeUpdated != null)
-                {
-                    // Update informations of object
-                    metricToBeUpdated.MetricName = metric.MetricName;
-                    metricToBeUpdated.MetricCategory = metric.MetricCategory;
-                    metricToBeUpdated.Description = metric.Description;
-                    metricToBeUpdated.FiscalYear = fyToBeIncluded;
-
-                    var response = _context.SaveChanges();
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                var response = _context.SaveChanges();
+                return true;
             }
-            catch (Exception)
+            else
             {
-                throw;
+                return false;
             }
         }
 
         // Delete metric based on ID
         public bool DeleteMetricByID(Guid id)
         {
-            try
+            var metricToBeDeleted = _context.Metrics.First(m => m.MetricID == id);
+
+            if (metricToBeDeleted != null)
             {
-                var metricToBeDeleted = _context.Metrics.First(m => m.MetricID == id);
+                var response = _context.Remove(metricToBeDeleted);
+                _context.SaveChanges();
 
-                if (metricToBeDeleted != null)
-                {
-                    var response = _context.Remove(metricToBeDeleted);
-                    _context.SaveChanges();
-
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return true;
             }
-            catch (Exception)
+            else
             {
-                throw;
+                return false;
             }
         }
     }
