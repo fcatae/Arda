84b1086a0f828e004fbae94496e87e059da635eb
Fabricio Catae <fabricio.catae@microsoft.com>
2017-02-22 18:25:44 -0300

    Remove unused function CallMicrosoftGraph (#4)

--- src/Arda.Main/Utils/TokenManager.cs
+++ src/Arda.Main/Utils/TokenManager.cs
@@ -19,52 +19,24 @@ namespace Arda.Main.Utils
             return await authContext.AcquireTokenAsync(Startup.GraphResourceId, credential);
         }
 
-        private static async Task<IActionResult> CallMicrosoftGraph(HttpContext context)
-        {
-            AuthenticationResult result = null;
+        //private static async Task<IActionResult> CallMicrosoftGraph(HttpContext context)
+        //{
+        //    AuthenticationResult result = null;
 
-            try
-            {
-                string userObjectID = context.User.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
-                AuthenticationContext authContext = new AuthenticationContext(Startup.Authority, new SessionCache(userObjectID, context));
-                ClientCredential credential = new ClientCredential(Startup.ClientId, Startup.ClientSecret);
-                result = await authContext.AcquireTokenSilentAsync(Startup.GraphResourceId, credential, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
-                return null;
-                //HttpClient client = new HttpClient();
-                //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/users/maluz@microsoft.com/photo/$value");
-                //request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
-                //HttpResponseMessage response = await client.SendAsync(request);
+        //    try
+        //    {
+        //        string userObjectID = context.User.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+        //        AuthenticationContext authContext = new AuthenticationContext(Startup.Authority, new SessionCache(userObjectID, context));
+        //        ClientCredential credential = new ClientCredential(Startup.ClientId, Startup.ClientSecret);
+        //        result = await authContext.AcquireTokenSilentAsync(Startup.GraphResourceId, credential, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
 
-                //if (response.IsSuccessStatusCode)
-                //{
-                //    var source = await response.Content.ReadAsByteArrayAsync();
-                //    return View();
-                //}
-                //else
-                //{
-                //    //
-                //    // If the call failed with access denied, then drop the current access token from the cache,
-                //    //     and show the user an error indicating they might need to sign-in again.
-                //    //
-                //    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                //    {
-
-                //        //var todoTokens = authContext.TokenCache.ReadItems().Where(a => a.Resource == Startup.TodoListResourceId);
-                //        //foreach (TokenCacheItem tci in todoTokens)
-                //        //    authContext.TokenCache.DeleteItem(tci);
-
-                //        //ViewBag.ErrorMessage = "UnexpectedError";
-                //    }
-                //}
-            }
-            catch (Exception)
-            {
-                //If get silent token fails:
-                return new ChallengeResult(OpenIdConnectDefaults.AuthenticationScheme);
-            }
-
-
-            //return View("Error");
-        }
+        //        return null;
+        //    }
+        //    catch (Exception)
+        //    {
+        //        //If get silent token fails: retry the auth
+        //        return new ChallengeResult(OpenIdConnectDefaults.AuthenticationScheme);
+        //    }
+        //}
     }
 }
