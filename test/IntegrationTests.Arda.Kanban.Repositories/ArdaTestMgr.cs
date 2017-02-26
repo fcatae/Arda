5b1a64782de18bca81fc045a8e1751106b53f3d3
Fabricio Catae <fcatae@microsoft.com>
2017-02-26 19:15:15 -0300

    Use a global connection string

---
+++ test/IntegrationTests.Arda.Kanban.Repositories/ArdaTestMgr.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Arda.Kanban.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace IntegrationTests
+{
+    public class ArdaTestMgr
+    {
+        private static IConfigurationRoot Configuration;
+
+        static ArdaTestMgr()
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", true)
+                .AddJsonFile("local-secret.json", true)
+                .AddJsonFile("microservices.json", true)
+                .AddUserSecrets("arda-20160816073715")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        static string GetConnectionString()
+        {
+            return Configuration["Storage:SqlServer-Kanban:ConnectionString"];
+        }
+
+        public static KanbanContext GetDbContext()
+        {
+            string connectionString = GetConnectionString();
+
+            var opts = (new DbContextOptionsBuilder<KanbanContext>())
+                            .UseSqlServer(connectionString);
+
+            return new KanbanContext(opts.Options);
+        }
+    }
+}
