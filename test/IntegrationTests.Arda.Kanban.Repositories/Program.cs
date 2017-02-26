74f873cb3cc7e54fc086ed09c764a65694900544
Fabricio Catae <fcatae@microsoft.com>
2017-02-26 20:57:46 -0300

    Use file checking

---
+++ test/IntegrationTests.Arda.Kanban.Repositories/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntegrationTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            ArdaTestMgr.AllowCreateResultFile = true;
+
+            var test = new FiscalYear();
+
+            test.FiscalYear_GetAllFiscalYears_Should_ReturnAllValues();
+        }
+    }
+}
