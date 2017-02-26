ad2d5f1c08f57ba4ff24f5997f9af1869057e038
Fabricio Catae <fcatae@microsoft.com>
2017-02-26 18:41:13 -0300

    add test project

---
+++ test/IntegrationTests.Arda.Kanban.Repositories/FiscalYear.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+using Arda.Kanban.Models;
+using Arda.Kanban.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegrationTests
+{
+    public class FiscalYear
+    {
+        KanbanContext GetDbContext()
+        {
+            var opts = (new DbContextOptionsBuilder<KanbanContext>())
+                            .UseSqlServer("connestring");
+
+            return new KanbanContext(opts.Options);
+        }
+
+        [Fact]
+        public void GetAllFiscalYears_Should_ReturnAllValues() 
+        {
+            var dbContext = GetDbContext();
+
+            FiscalYearRepository fiscalYear = new FiscalYearRepository(dbContext);
+
+            var list = fiscalYear.GetAllFiscalYears();
+
+            Assert.True(true);
+        }
+    }
+}
