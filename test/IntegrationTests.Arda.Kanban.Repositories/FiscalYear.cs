cda1794c37388c34ebf75fe58c471138f776829d
Fabricio Catae <fcatae@microsoft.com>
2017-02-26 21:04:46 -0300

    cleanup

--- test/IntegrationTests.Arda.Kanban.Repositories/FiscalYear.cs
+++ test/IntegrationTests.Arda.Kanban.Repositories/FiscalYear.cs
@@ -12,31 +12,11 @@ namespace IntegrationTests
         [Fact]
         public void FiscalYear_GetAllFiscalYears_Should_ReturnAllValues() 
         {
-            var dbContext = ArdaTestMgr.GetDbContext();
-
-            FiscalYearRepository fiscalYear = new FiscalYearRepository(dbContext);
+            FiscalYearRepository fiscalYear = new FiscalYearRepository(ArdaTestMgr.GetDbContext());
 
             var list = fiscalYear.GetAllFiscalYears();
 
             ArdaTestMgr.CheckResult(list);
-
-            //string result = ArdaTestMgr.SerializeObject(list);
-            //ArdaTestMgr.WriteFile("GetAllFiscalYears_Should_ReturnAllValues.json", result);
-            //string expected = ArdaTestMgr.ReadFile("GetAllFiscalYears_Should_ReturnAllValues.json");           
-            //Assert.Equal(result, expected);
-        }
-
-        void GetFile(IEnumerable<Arda.Common.ViewModels.Main.FiscalYearViewModel> models)
-        {
-            var res = from ma in models
-                      orderby ma.FiscalYearID
-                      select ma;
-                                
-
-            foreach (var m in models)
-            {
-
-            }
         }
     }
 }
