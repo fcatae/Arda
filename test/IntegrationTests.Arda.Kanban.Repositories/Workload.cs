2623d907e7d11afff9f3ed75096f6c475bbee668
Fabricio Catae <fcatae@microsoft.com>
2017-02-27 11:50:26 -0300

    create a sample workload

--- test/IntegrationTests.Arda.Kanban.Repositories/Workload.cs
+++ test/IntegrationTests.Arda.Kanban.Repositories/Workload.cs
@@ -32,21 +32,41 @@ namespace IntegrationTests
         [Fact]
         public void Workload_AddNewWorkload_Should_AddRow()
         {
+            WorkloadViewModel WORKLOAD1 = new WorkloadViewModel()
+            {
+                WBActivity = Guid.Parse("1f265df5-adbe-4b7b-a05a-451af058c482"), // POC
+                WBComplexity = 1,
+                WBCreatedBy = "admin@ardademo.onmicrosoft.com",
+                WBCreatedDate = DateTime.Parse("2021-01-20"),
+                WBEndDate = DateTime.Parse("2021-01-30"),
+                WBDescription = "My Workload Description",
+                WBExpertise = 2,
+                WBFilesList = null,
+                WBID = Guid.Parse("aaaa0022-FD15-428C-9B24-14E6467977AD"),
+                WBIsWorkload = true,
+                WBMetrics = new Guid[] {
+                    Guid.Parse("6da887cb-9edd-42cb-87c9-83ac772d9b65"), // Community
+                    Guid.Parse("45979112-aff6-4bfa-878b-02baa8fd1074")  // Education
+                },
+                WBStartDate = DateTime.Parse("2021-01-25"),
+                WBStatus = 3,
+                WBTechnologies = new Guid[] {
+                    Guid.Parse("9c263d44-2c11-48cd-b876-5ebb540bbf51"), // Infra
+                    Guid.Parse("af5d8796-0ca2-4d54-84f7-d3194f5f2426")  // Web & Mobile
+                },
+                WBTitle = "My Initial Workload",
+                WBUsers = new string[] {
+                    "user@ardademo.onmicrosoft.com", "admin@ardademo.onmicrosoft.com"
+                }
+            };
+
             ArdaTestMgr.Validate(this, $"Workload.AddNewWorkload()",
                 (list, ctx) => {
                     WorkloadRepository workload = new WorkloadRepository(ctx);
+                    
+                    workload.AddNewWorkload(WORKLOAD1);
 
-                    var rows = from r in list
-                               select r.WBTitle;
-
-                    var row = new WorkloadViewModel()
-                    {
-                        // TODO: fill
-                    };
-
-                    workload.AddNewWorkload(row);
-
-                    return rows;
+                    return workload.GetAllWorkloads();
                 });
         }
         
