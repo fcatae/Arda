f57622dd2151589f214f83761aeb667c59d4c190
Fabricio Catae <fcatae@microsoft.com>
2017-02-27 11:03:00 -0300

    User_AddNewUser_Should_AddRow

---
+++ test/IntegrationTests.Arda.Kanban.Repositories/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using System.Collections.Generic;
+using Arda.Kanban.Models;
+using Arda.Kanban.Repositories;
+using Arda.Common.ViewModels.Kanban;
+
+namespace IntegrationTests
+{
+    public class User : ISupportSnapshot<UserKanbanViewModel>
+    {
+        public IEnumerable<UserKanbanViewModel> GetSnapshot(TransactionalKanbanContext context)
+        {
+            UserRepository user = new UserRepository(context);
+
+            return user.GetAllUsers().ToArray();
+        }
+
+        [Fact]
+        public void User_AddNewUser_Should_AddRow()
+        {
+            string USER_UNIQUENAME = "newuser@domain.com";
+            string USER_NAME = "New User 1";
+
+
+            ArdaTestMgr.Validate(this, $"User.AddNewUser({USER_UNIQUENAME},{USER_NAME})",
+                (list, ctx) => {
+                    UserRepository user = new UserRepository(ctx);
+
+                    var row = new UserKanbanViewModel()
+                    {
+                        UniqueName = USER_UNIQUENAME,
+                        Name = USER_NAME
+                    };
+
+                    user.AddNewUser(row);
+
+                    return user.GetAllUsers();
+                });
+        }
+
+        //[Fact]
+        //public void User_AddNewFiscalYear_Should_AddRow()
+        //{
+        //    string GUID = "{aaaa0000-622a-4656-85df-39edc26be080}";
+
+        //    ArdaTestMgr.Validate(this, $"User.Bla({GUID})",
+        //        (list, ctx) => {
+        //            UserRepository user = new UserRepository(ctx);
+
+        //            user.AddNewUser(row);
+
+        //            return user.GetAllUsers();
+        //        });
+        //}
+
+    }
+}
