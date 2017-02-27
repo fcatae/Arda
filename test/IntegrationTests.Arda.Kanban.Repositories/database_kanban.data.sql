605337dcc544308cd2781ba4925270c2044b421d
Fabricio Catae <fcatae@microsoft.com>
2017-02-27 10:49:55 -0300

    organized sql data

--- test/IntegrationTests.Arda.Kanban.Repositories/database_kanban.data.sql
+++ test/IntegrationTests.Arda.Kanban.Repositories/database_kanban.data.sql
@@ -1,12 +1,21 @@
-INSERT [dbo].[Activities] ([ActivityID], [ActivityName]) VALUES (N'cd8dc5ec-e200-4290-9faa-1127b65c51a3', N'Presentation')
-INSERT [dbo].[Activities] ([ActivityID], [ActivityName]) VALUES (N'd33c434e-a007-4eec-b0b6-16c505b721ff', N'Hackatons')
-INSERT [dbo].[Activities] ([ActivityID], [ActivityName]) VALUES (N'e23d098e-013b-4afe-a966-1f515c9a1a12', N'POC')
-INSERT [dbo].[FiscalYears] ([FiscalYearID], [FullNumericFiscalYear], [TextualFiscalYear]) VALUES (N'e0fd2d01-020e-475f-9c28-a90f5d857877', 2017, N'FY17')
+delete metrics
+delete fiscalyears
+delete activities
+delete technologies
+
+INSERT [dbo].[FiscalYears] ([FiscalYearID], [FullNumericFiscalYear], [TextualFiscalYear]) VALUES (N'D8043C31-EBA9-41C8-81A1-EBC828D81DCB', 2016, N'FY16')
+INSERT [dbo].[FiscalYears] ([FiscalYearID], [FullNumericFiscalYear], [TextualFiscalYear]) VALUES (N'D38759AB-E310-46F0-A6C3-B0594C2531AB', 2017, N'FY17')
 INSERT [dbo].[FiscalYears] ([FiscalYearID], [FullNumericFiscalYear], [TextualFiscalYear]) VALUES (N'C6A45416-81A2-4034-ADAC-C7EAB5225ECE', 2018, N'FY18')
-INSERT [dbo].[Metrics] ([MetricID], [Description], [FiscalYearID], [MetricCategory], [MetricName]) VALUES (N'821acb1a-2761-48a6-9e18-509c67a06c80', N'Workshops and Events', N'e0fd2d01-020e-475f-9c28-a90f5d857877', N'ISVs', N'Evangelism')
-INSERT [dbo].[Metrics] ([MetricID], [Description], [FiscalYearID], [MetricCategory], [MetricName]) VALUES (N'7479b502-74b9-4673-9e2f-d35f7fd19edd', N'Customer satisfaction.', N'e0fd2d01-020e-475f-9c28-a90f5d857877', N'Community', N'Customer SAT')
-INSERT [dbo].[Metrics] ([MetricID], [Description], [FiscalYearID], [MetricCategory], [MetricName]) VALUES (N'819193e6-ea01-4c4e-a948-fc44453b2604', N'Azure training for students', N'e0fd2d01-020e-475f-9c28-a90f5d857877', N'Education', N'Azure Training')
-INSERT [dbo].[Technologies] ([TechnologyID], [TechnologyName]) VALUES (N'f938b4ba-bdf3-4d07-b5ca-8389e7256460', N'Microsoft Azure: Analytics')
-INSERT [dbo].[Technologies] ([TechnologyID], [TechnologyName]) VALUES (N'f28c67ff-5250-45b5-a750-9ba5ec580752', N'Microsoft Azure: Infra')
-INSERT [dbo].[Technologies] ([TechnologyID], [TechnologyName]) VALUES (N'230fa523-917a-4076-b58d-c4022f187bb7', N'Microsoft Azure: IoT')
-INSERT [dbo].[Technologies] ([TechnologyID], [TechnologyName]) VALUES (N'2edba5ef-721f-4d9f-a1a5-010beafb2812', N'Microsoft Azure: Web & Mobile')
+
+INSERT [dbo].[Metrics] ([MetricID], [Description], [FiscalYearID], [MetricCategory], [MetricName]) VALUES (N'20297652-9BE6-4560-AA36-586F99893FEF', N'Workshops and Events', N'D8043C31-EBA9-41C8-81A1-EBC828D81DCB', N'ISVs', N'Evangelism')
+INSERT [dbo].[Metrics] ([MetricID], [Description], [FiscalYearID], [MetricCategory], [MetricName]) VALUES (N'6DA887CB-9EDD-42CB-87C9-83AC772D9B65', N'Customer satisfaction.', N'D38759AB-E310-46F0-A6C3-B0594C2531AB', N'Community', N'Customer SAT')
+INSERT [dbo].[Metrics] ([MetricID], [Description], [FiscalYearID], [MetricCategory], [MetricName]) VALUES (N'45979112-AFF6-4BFA-878B-02BAA8FD1074', N'Azure training for students', N'D38759AB-E310-46F0-A6C3-B0594C2531AB', N'Education', N'Azure Training')
+
+INSERT [dbo].[Activities] ([ActivityID], [ActivityName]) VALUES (N'06C15279-9AFB-4723-AF61-33EAF8974D47', N'Presentation')
+INSERT [dbo].[Activities] ([ActivityID], [ActivityName]) VALUES (N'D23D46AC-9D8C-43F5-B673-8B169B04081F', N'Hackatons')
+INSERT [dbo].[Activities] ([ActivityID], [ActivityName]) VALUES (N'1F265DF5-ADBE-4B7B-A05A-451AF058C482', N'POC')
+
+INSERT [dbo].[Technologies] ([TechnologyID], [TechnologyName]) VALUES (N'B2B1BCE9-DF05-4FBF-ABB7-89617D396B4C', N'Analytics')
+INSERT [dbo].[Technologies] ([TechnologyID], [TechnologyName]) VALUES (N'9C263D44-2C11-48CD-B876-5EBB540BBF51', N'Infra')
+INSERT [dbo].[Technologies] ([TechnologyID], [TechnologyName]) VALUES (N'CEC4B7BB-4F14-4118-AE31-29BC39AE93D8', N'IoT')
+INSERT [dbo].[Technologies] ([TechnologyID], [TechnologyName]) VALUES (N'AF5D8796-0CA2-4D54-84F7-D3194F5F2426', N'Web & Mobile')
