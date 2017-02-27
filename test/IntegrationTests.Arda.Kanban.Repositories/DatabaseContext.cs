ba0e999ca735ca762740ac95b0e8d86fba93b767
Fabricio Catae <fcatae@microsoft.com>
2017-02-26 22:14:47 -0300

    Add a transactional scope

---
+++ test/IntegrationTests.Arda.Kanban.Repositories/DatabaseContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Arda.Kanban.Models;
+using System.Data.SqlClient;
+
+namespace IntegrationTests
+{
+    public class TransactionalKanbanContext : KanbanContext, IDisposable
+    {
+        private SqlConnection _connection;
+
+        public static TransactionalKanbanContext Create(string connectionString)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            SqlCommand sqlcmd = new SqlCommand("SET IMPLICIT_TRANSACTIONS ON", connection);
+            sqlcmd.ExecuteNonQuery();
+
+            var opts = (new DbContextOptionsBuilder<KanbanContext>())
+                            .UseSqlServer(connection);
+
+            return new TransactionalKanbanContext(opts.Options, connection);
+        }
+
+        public TransactionalKanbanContext(DbContextOptions<KanbanContext> options, SqlConnection connection) : base(options)
+        {
+            this._connection = connection;
+        }
+
+        public new void Dispose()
+        {
+            if(_connection != null)
+            {
+                _connection.Dispose();
+                _connection = null;
+            }
+
+            base.Dispose();
+        }        
+    }
+}
