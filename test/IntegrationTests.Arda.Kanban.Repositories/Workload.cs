e24a94c6eb2d7c45c5c607b5bd40e25660028c24
Fabricio Catae <fcatae@microsoft.com>
2017-02-27 13:16:31 -0300

    Add GUID for testing

--- test/IntegrationTests.Arda.Kanban.Repositories/Workload.cs
+++ test/IntegrationTests.Arda.Kanban.Repositories/Workload.cs
@@ -73,7 +73,7 @@ namespace IntegrationTests
         [Fact]
         public void Workload_DeleteWorkloadByID_Should_ReturnRemoveExactlyOne()
         {
-            string GUID = "{...}";
+            string GUID = "{C1507019-1D97-4629-8015-C01BF02CE6AB}";
 
             ArdaTestMgr.Validate(this, $"Workload.DeleteWorkloadByID({GUID})",
                 (list, ctx) => {
@@ -88,7 +88,7 @@ namespace IntegrationTests
         [Fact]
         public void Workload_GetWorkloadByID_Should_ReturnExactlyOne()
         {
-            string GUID = "{...}";
+            string GUID = "{C1507019-1D97-4629-8015-C01BF02CE6AB}";
 
             ArdaTestMgr.Validate(this, $"Workload.GetWorkloadByID({GUID})",
                 (list, ctx) => {
@@ -118,37 +118,101 @@ namespace IntegrationTests
         [Fact]
         public void Workload_EditWorkload_Should_ChangeRow()
         {
-            string GUID = "{...}";
+            string GUID = "{90cac674-18c0-4139-8aae-f9711bd2d5f4}";
+            
+            WorkloadViewModel WORKLOAD1 = new WorkloadViewModel()
+            {
+                WBActivity = Guid.Parse("1f265df5-adbe-4b7b-a05a-451af058c482"), // POC
+                WBComplexity = 1,
+                WBCreatedBy = "admin@ardademo.onmicrosoft.com",
+                WBCreatedDate = DateTime.Parse("2021-01-20"),
+                WBEndDate = DateTime.Parse("2021-01-30"),
+                WBDescription = "My Workload Description",
+                WBExpertise = 2,
+                WBFilesList = null,
+                WBID = Guid.Parse("aaaa0022-FD15-428C-9B24-14E6467977AD"),
+                WBIsWorkload = true,
+                WBMetrics = new Guid[] {
+                    Guid.Parse("6da887cb-9edd-42cb-87c9-83ac772d9b65"), // Community
+                    Guid.Parse("45979112-aff6-4bfa-878b-02baa8fd1074")  // Education
+                },
+                WBStartDate = DateTime.Parse("2021-01-25"),
+                WBStatus = 3,
+                WBTechnologies = new Guid[] {
+                    Guid.Parse("9c263d44-2c11-48cd-b876-5ebb540bbf51"), // Infra
+                    Guid.Parse("af5d8796-0ca2-4d54-84f7-d3194f5f2426")  // Web & Mobile
+                },
+                WBTitle = "My Initial Workload",
+                WBUsers = new string[] {
+                    "user@ardademo.onmicrosoft.com", "admin@ardademo.onmicrosoft.com"
+                }
+            };
 
             ArdaTestMgr.Validate(this, $"Workload.EditWorkload({GUID})",
                 (list, ctx) => {
                     WorkloadRepository workload = new WorkloadRepository(ctx);
 
-                    var row = (from r in list
-                               where r.WBID == Guid.Parse(GUID)
-                               select r).First();
+                    var row = WORKLOAD1;
 
-                    // edit
+                    row.WBID = Guid.Parse(GUID);
 
                     workload.EditWorkload(row);
 
                     return workload.GetAllWorkloads();
                 });
-        }        
+        }
 
+        [Fact]
+        public void Workload_EditWorkload_Should_NotFailWithEmptyMetrics()
+        {
+            string GUID = "{C1507019-1D97-4629-8015-C01BF02CE6AB}";
+
+            WorkloadViewModel WORKLOAD1 = new WorkloadViewModel()
+            {
+                WBActivity = Guid.Parse("1f265df5-adbe-4b7b-a05a-451af058c482"), // POC
+                WBComplexity = 1,
+                WBCreatedBy = "admin@ardademo.onmicrosoft.com",
+                WBCreatedDate = DateTime.Parse("2021-01-20"),
+                WBEndDate = DateTime.Parse("2021-01-30"),
+                WBDescription = "My Workload Description",
+                WBExpertise = 2,
+                WBFilesList = null,
+                WBID = Guid.Parse("aaaa0022-FD15-428C-9B24-14E6467977AD"),
+                WBIsWorkload = true,
+                WBMetrics = null,
+                WBStartDate = DateTime.Parse("2021-01-25"),
+                WBStatus = 3,
+                WBTechnologies = null,
+                WBTitle = "My Initial Workload",
+                WBUsers = null
+            };
+
+            ArdaTestMgr.Validate(this, $"Workload.EditWorkload({GUID})",
+                (list, ctx) => {
+                    WorkloadRepository workload = new WorkloadRepository(ctx);
+
+                    var row = WORKLOAD1;
+
+                    row.WBID = Guid.Parse(GUID);
+
+                    workload.EditWorkload(row);
+
+                    return workload.GetAllWorkloads();
+                });
+        }
         [Fact]
         public void Workload_UpdateWorkloadStatus_Should_UpdateOneStatus()
         {
-            string GUID = "{...}";
+            string GUID = "{C1507019-1D97-4629-8015-C01BF02CE6AB}";
             int STATUS = 4;
 
             ArdaTestMgr.Validate(this, $"Workload.UpdateWorkloadStatus({GUID},{STATUS})",
                 (list, ctx) => {
                     WorkloadRepository workload = new WorkloadRepository(ctx);
 
-                    var row = workload.UpdateWorkloadStatus(Guid.Parse(GUID), STATUS);
+                    workload.UpdateWorkloadStatus(Guid.Parse(GUID), STATUS);
 
-                    return row;
+                    return workload.GetAllWorkloads();
                 });
         }
 
