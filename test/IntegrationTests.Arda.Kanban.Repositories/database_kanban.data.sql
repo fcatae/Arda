21ee4131bcb4f883107d2c216282384e6830bce2
Fabricio Catae <fcatae@microsoft.com>
2017-02-27 12:11:12 -0300

    add workload rows to db

--- test/IntegrationTests.Arda.Kanban.Repositories/database_kanban.data.sql
+++ test/IntegrationTests.Arda.Kanban.Repositories/database_kanban.data.sql
@@ -23,3 +23,20 @@ INSERT [dbo].[Technologies] ([TechnologyID], [TechnologyName]) VALUES (N'AF5D879
 INSERT [Users](UniqueName, Name) VALUES ('user@ardademo.onmicrosoft.com', 'User1')
 INSERT [Users](UniqueName, Name) VALUES ('admin@ardademo.onmicrosoft.com', 'Admin')
 INSERT [Users](UniqueName, Name) VALUES ('guest@ardademo.onmicrosoft.com', 'Guest User')
+
+
+INSERT [WorkloadBacklogs] (WBID, WBTitle, WBDescription, WBCreatedBy, WBActivityActivityID, WBCreatedDate, WBEndDate, WBStartDate, WBComplexity, WBExpertise, WBStatus, WBIsWorkload) VALUES
+('90CAC674-18C0-4139-8AAE-F9711BD2D5F4', 'First commit', 'Blah 1', 'user@ardademo.onmicrosoft.com', '1F265DF5-ADBE-4B7B-A05A-451AF058C482', '2016-01-01', '2016-12-31', '2016-01-02', 0, 1, 2, 0),
+('C1507019-1D97-4629-8015-C01BF02CE6AB', 'Attend Event', 'Blah 2', 'admin@ardademo.onmicrosoft.com', '06C15279-9AFB-4723-AF61-33EAF8974D47', '2017-01-01', '2017-12-31', '2017-01-15', 3, 2, 1, 1)
+
+DECLARE @WBID UNIQUEIDENTIFIER = 'C1507019-1D97-4629-8015-C01BF02CE6AB'
+
+INSERT [WorkloadBacklogMetrics] (WBMetricID, MetricMetricID, WorkloadBacklogWBID) VALUES ('BB11B40A-6231-44D2-8F49-B6FE3460ADBA', '20297652-9BE6-4560-AA36-586F99893FEF', @WBID)
+INSERT [WorkloadBacklogMetrics] (WBMetricID, MetricMetricID, WorkloadBacklogWBID) VALUES ('95F2ABE8-439C-45F9-ABCA-0010F8C3A83C', '45979112-AFF6-4BFA-878B-02BAA8FD1074', @WBID)
+
+INSERT [WorkloadBacklogTechnologies] (WBUTechnologyID, TechnologyTechnologyID, WorkloadBacklogWBID) VALUES ('200FBE3B-6361-4287-994B-AB34AFF1C0CE', 'B2B1BCE9-DF05-4FBF-ABB7-89617D396B4C', @WBID)
+INSERT [WorkloadBacklogTechnologies] (WBUTechnologyID, TechnologyTechnologyID, WorkloadBacklogWBID) VALUES ('50F71F81-DBB7-413E-BEA2-F69CB8E162DF', 'CEC4B7BB-4F14-4118-AE31-29BC39AE93D8', @WBID)
+
+INSERT [WorkloadBacklogUsers] (WBUserID, UserUniqueName, WorkloadBacklogWBID) VALUES ('7764CDDE-F51D-41AC-B1B1-11B2A1D485BA', 'user@ardademo.onmicrosoft.com', @WBID)
+INSERT [WorkloadBacklogUsers] (WBUserID, UserUniqueName, WorkloadBacklogWBID) VALUES ('169E3201-282B-4740-A372-1CE228C68348', 'admin@ardademo.onmicrosoft.com', @WBID)
+INSERT [WorkloadBacklogUsers] (WBUserID, UserUniqueName, WorkloadBacklogWBID) VALUES ('BC7D605B-21FB-4182-91DB-D968C71D8E6A', 'guest@ardademo.onmicrosoft.com', @WBID)
