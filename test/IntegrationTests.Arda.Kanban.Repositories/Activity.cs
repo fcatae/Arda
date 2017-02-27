473efe8d124efbe0a30f86570d294840304e3b28
Fabricio Catae <fcatae@microsoft.com>
2017-02-27 10:49:22 -0300

    Actitivy_GetAllActivities_Should_ReturnAllValues

---
+++ test/IntegrationTests.Arda.Kanban.Repositories/Activity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using System.Collections.Generic;
+using Arda.Kanban.Models;
+using Arda.Kanban.Repositories;
+using Arda.Common.ViewModels.Main;
+
+namespace IntegrationTests
+{
+    public class Actitivy : ISupportSnapshot<ActivityViewModel>
+    {
+        public IEnumerable<ActivityViewModel> GetSnapshot(TransactionalKanbanContext context)
+        {
+            ActivityRepository activiy = new ActivityRepository(context);
+
+            return activiy.GetAllActivities().ToArray();
+        }
+
+        [Fact]
+        public void Actitivy_GetAllActivities_Should_ReturnAllValues() 
+        {
+            ArdaTestMgr.Validate(this, $"Actitivy.GetAllActivities()",
+                (list, ctx) => {
+                    var rows = from r in list
+                               select r.ActivityName;
+
+                    return rows;
+                });
+        }        
+    }
+}
