b3ef9de60d23a894c4a9de96f238bbcfd8585888
Fabricio Catae <fcatae@microsoft.com>
2017-02-27 00:04:54 -0300

    generalize the code

---
+++ test/IntegrationTests.Arda.Kanban.Repositories/TableSnapshot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    public interface ISupportSnapshot<R>
+    {
+        IEnumerable<R> GetSnapshot(TransactionalKanbanContext context);
+    }
+
+    public class TableSnapshot
+    {
+    }
+}
