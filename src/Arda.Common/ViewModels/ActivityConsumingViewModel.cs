4a1565e7cfdbab59e407bbd729b1f0f1bda83f18
Fabricio Catae <fabricio.catae@microsoft.com>
2017-03-07 14:13:02 -0300

    fixed arda.main

---
+++ src/Arda.Common/ViewModels/ActivityConsumingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Arda.Common.ViewModels.Reports
+{
+    public class ActivityConsumingViewModel
+    {
+        public string Activity { get; set; }
+
+        public int Hours { get; set; }
+
+        public decimal Percent { get; set; }
+    }
+}
