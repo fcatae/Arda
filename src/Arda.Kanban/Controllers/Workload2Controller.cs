b1993a9dbd9c3f1ea418ceca295c18fefc8cfb25
Fabricio Catae <fcatae@microsoft.com>
2017-04-27 21:22:33 -0300

    fix the name

---
+++ src/Arda.Kanban/Controllers/Workload2Controller.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+using Arda.Kanban.Models.Repositories;
+using Arda.Kanban.ViewModels;
+
+namespace Arda.Kanban.Controllers
+{
+    public class AssignInput
+    {
+        public string tag { get; set; }
+        public string wbid { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    public class Workload2Controller : Controller
+    {
+        IWorkloadRepository _repository;
+        Repositories.WorkloadExtraRepository _extraRepo;
+
+        Models.KanbanContext _context;
+
+        public Workload2Controller(IWorkloadRepository repository, Models.KanbanContext context)
+        {
+            _repository = repository;
+            _context = context;
+            _extraRepo = new Repositories.WorkloadExtraRepository(context);
+        }
+
+        [HttpGet]
+        [Route("listtag")]
+        public IEnumerable<WorkloadsByUserViewModel> ListTag([FromQuery]string tag)
+        {
+            return _extraRepo.GetWorkloads(tag);
+        }
+
+        [HttpPost]
+        [Route("assign")]
+        public bool Assign([FromBody]AssignInput input)
+        {
+            _extraRepo.AssignTag(Guid.Parse(input.wbid), input.tag);
+
+            return true;
+        }
+
+        [HttpGet]
+        [Route("listworkloadwithfilter")]
+        public IEnumerable<WorkloadsByUserViewModel> ListWorkloadWithFilter([FromQuery]string tag)
+        {
+            return _extraRepo.GetWorkloads(tag);
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IEnumerable<WorkloadViewModel> List()
+        {
+            return _extraRepo.GetWorkloads("tag");
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public HttpResponseMessage Add()
+        {
+            var uniqueName = HttpContext.Request.Headers["unique_name"].ToString();
+
+            System.IO.StreamReader reader = new System.IO.StreamReader(HttpContext.Request.Body);
+            string requestFromPost = reader.ReadToEnd();
+            var workload = JsonConvert.DeserializeObject<WorkloadViewModel>(requestFromPost);
+
+            // Calling add
+            var response = _repository.AddNewWorkload(workload);
+
+            if (response)
+            {
+                _repository.SendNotificationAboutNewOrUpdatedWorkload(uniqueName, 0);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
