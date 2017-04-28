f93590163f7fdff41070395f508f04c43d7a1009
Fabricio Catae <fcatae@users.noreply.github.com>
2017-04-28 16:44:24 -0300

    Release April (#76)
    * Support Workspaces (tag)

---
+++ src/Arda.Kanban/Models/WorkloadBacklogTags.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Arda.Kanban.Models
+{
+    [Table("WorkloadBacklogTags")]
+    public class WorkloadBacklogTags
+    {
+        [Key]
+        [Required]
+        public string TagId { get; set; }
+
+        public Guid WorkloadBacklogWBID { get; set; }
+
+        [ForeignKey("WorkloadBacklogWBID")]
+        public virtual WorkloadBacklog WorkloadBacklog { get; set; }
+    }
+}
---
+++ src/Arda.Kanban/Repositories/WorkloadExtraRepository.cs
@@ -0,0 +1,62 @@
+﻿using Arda.Kanban.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Arda.Kanban.Models.Repositories;
+using Arda.Kanban.ViewModels;
+
+namespace Arda.Kanban.Repositories
+{
+    public class WorkloadExtraRepository // : IWorkloadExtraRepository
+    {
+        KanbanContext _context;
+
+        public WorkloadExtraRepository(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        public void AssignTag(Guid wbid, string tag)
+        {
+            WorkloadBacklogTags workloadTag = new WorkloadBacklogTags()
+            {
+                TagId = tag,
+                WorkloadBacklogWBID = wbid
+            };
+
+            _context.Tags.Add(workloadTag);
+            _context.SaveChanges();
+        }
+        
+        public IEnumerable<WorkloadsByUserViewModel> GetWorkloads(string uniqueName)
+        {
+            var workloads = (from wb in _context.WorkloadBacklogs
+                             join tag in _context.Tags on wb.WBID equals tag.WorkloadBacklogWBID
+
+                             // where (int)wb.WBStatus < 3
+                             where tag.TagId == uniqueName
+                             orderby wb.WBTitle
+                             select new WorkloadsByUserViewModel
+                             {
+                                 _WorkloadID = wb.WBID,
+                                 _WorkloadTitle = wb.WBTitle,
+                                 _WorkloadStartDate = wb.WBStartDate,
+                                 _WorkloadEndDate = wb.WBEndDate,
+                                 _WorkloadStatus = (int)wb.WBStatus,
+                                 _WorkloadIsWorkload = wb.WBIsWorkload,
+                                 _WorkloadAttachments = wb.WBFiles.Count,
+                                 _WorkloadExpertise = wb.WBExpertise.ToString(),
+                                 _WorkloadUsers = (from wbusers in _context.WorkloadBacklogUsers
+                                                   where wbusers.WorkloadBacklog.WBID == wb.WBID
+                                                   select new Tuple<string, string>(wbusers.User.UniqueName, wbusers.User.Name)).ToList(),
+
+                                 _WorkloadHours = 0 // ignore
+
+                             }).ToList();
+
+
+            return workloads;
+        }
+        
+    }
+}
