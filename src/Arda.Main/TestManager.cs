7e94ad34b2081d7910f2d718fe8d36880a378257
Fabricio Catae <fcatae@microsoft.com>
2017-05-10 00:29:26 -0300

    Tests are robust

--- src/Arda.Main/TestManager.cs
+++ src/Arda.Main/TestManager.cs
@@ -23,46 +23,120 @@ namespace Arda.Main
 
             var test = new TestManager();
 
-            test.TestMain();
+            test.WorkspaceItem_EditItem();
+            test.WorkspaceItem_PatchEdit();
+            test.WorkspaceItem_UpdateStatus();
+
         }
 
         public void TestMain()
         {
-            // Add a workload
-            object result = _client.WorkspaceFoldersService.AddItem(_user, new AddItemInput() { Title = "Test to be removed", ItemState = 2 });
+        }
 
-            // force the Cast
-            WorkspaceItem workload = (WorkspaceItem)result;
+        void WorkspaceItem_EditItem()
+        {
+            bool success = TestEdit(TestWorkspaceEdit);
 
-            // check if not null
+            if (!success)
+                throw new InvalidOperationException("TestEdit(TestWorkspaceEdit)");
+        }
+
+        void WorkspaceItem_PatchEdit()
+        {
+            bool success = TestEdit(TestWorkspacePatchEdit);
+
+            if (!success)
+                throw new InvalidOperationException("TestEdit(TestWorkspacePatchEdit)");
+        }
+
+        void WorkspaceItem_UpdateStatus()
+        {
+            bool success = TestEdit(TestWorkspaceUpdateStatus);
+
+            if (!success)
+                throw new InvalidOperationException("TestEdit(TestWorkspaceUpdateStatus)");
+        }
+
+        bool TestEdit(Func<WorkspaceItem,bool> testEditFunc)
+        {
+            var item = new AddItemInput() { Title = "Test Edit", ItemState = 0 };
+            bool success = false;
+
+            // Add 
+            WorkspaceItem workload = (WorkspaceItem)_client.WorkspaceFoldersService.AddItem(_user, item);
             Guid workloadId = workload.Id.Value;
 
-            // Delete the workload
-            _client.WorkspaceItemsService.Delete(workloadId);
+            try
+            {
+                success = testEditFunc(workload);
+            }
+            finally
+            {
+                _client.WorkspaceItemsService.Delete(workloadId);
+            }
 
+            return success;
         }
 
-        public void WorkspaceFolders_AddItems()
+        bool TestWorkspaceEdit(WorkspaceItem workload)
         {
-            // Add Test with different properties
-            _client.WorkspaceFoldersService.AddItem(_user, new AddItemInput() { Title = "Test 1" });
-            _client.WorkspaceFoldersService.AddItem(_user, new AddItemInput() { Title = "Test 2", Description = "123" });
-            _client.WorkspaceFoldersService.AddItem(_user, new AddItemInput() { Title = "Test 3", ItemState = 2 });
+            string START = "!";
+            string DESCRIPTION1 = "First Description";
+
+            Guid workloadId = workload.Id.Value;
+
+            workload.Title = DESCRIPTION1;
+            workload.Description = START + DESCRIPTION1;
+            workload.ItemState = 1;
+
+            // Edit
+            _client.WorkspaceItemsService.Edit(workload);
+
+            var result1 = _client.WorkspaceItemsService.GetItem(workloadId);
+
+            return  (result1.Title == DESCRIPTION1) &&
+                    (result1.Description == START + DESCRIPTION1) &&
+                    (result1.ItemState == 1);            
         }
 
-        public void WorkspaceFolders_ListItems()
+        bool TestWorkspacePatchEdit(WorkspaceItem workload)
         {
-            string user = "fcatae@microsoft.com";
-            string ARDA = "arda";
+            string START = "!";
+            string DESCRIPTION2 = "Multiple Descriptions";
 
-            // Show the current dashboard
-            var dashboardItems = _client.WorkspaceFoldersService.ListItems(user);
+            Guid workloadId = workload.Id.Value;
 
-            var archivedItems = _client.WorkspaceFoldersService.ListItems(user, type: "archived");
+            // PatchEdit 
+            _client.WorkspaceItemsService.EditItem(workloadId, new EditItemInput() { Title = DESCRIPTION2 });
+            _client.WorkspaceItemsService.EditItem(workloadId, new EditItemInput() { Description = START + DESCRIPTION2 });
+            _client.WorkspaceItemsService.EditItem(workloadId, new EditItemInput() { ItemState = 2 });
 
-            var backlogItems = _client.WorkspaceFoldersService.ListItems(user, type: "backlog");
+            // Check
+            var result1 = _client.WorkspaceItemsService.GetItem(workloadId);
 
-            var taggedItems = _client.WorkspaceFoldersService.ListItems(ARDA, type: "tag");
+            return  (result1.Title == DESCRIPTION2) &&
+                    (result1.Description == START + DESCRIPTION2) &&
+                    (result1.ItemState == 2);           
         }
+
+        bool TestWorkspaceUpdateStatus(WorkspaceItem workload)
+        {
+            Guid workloadId = workload.Id.Value;
+
+            var wis = _client.WorkspaceItemsService;
+
+            for(int n=0; n<3; n++)
+            {
+                // Update Status to 'n'
+                wis.UpdateStatus(workloadId, n);
+
+                // Check failure
+                if (wis.GetItem(workloadId).ItemState != n)
+                    return false;
+            }
+
+            return true;
+        }
+
     }
 }
